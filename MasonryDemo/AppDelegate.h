6cce59d19f2652f10a470d889d17ecee330d5f26
yimouleng <wmt443040748@163.com>
2015-12-06 14:48:26 +0800

    Initial Commit

---
+++ MasonryDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MasonryDemo
+//
+//  Created by Eli on 15/12/6.
+//  Copyright © 2015年 Ely. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MasonryDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MasonryDemo
+//
+//  Created by Eli on 15/12/6.
+//  Copyright © 2015年 Ely. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
